2ff459f268f1aa40d6b29ce12ccb4dfa28c1a539
Antonioafj <antonioafj.edu@gmail.com>
2025-09-13 13:26:26 -0400

    criado busca com id no controller

--- product-ms/src/main/java/br/com/youtube/product_ms/controller/ProductController.java
+++ product-ms/src/main/java/br/com/youtube/product_ms/controller/ProductController.java
@@ -3,14 +3,14 @@ package br.com.youtube.product_ms.controller;
 
 import br.com.youtube.product_ms.dto.ProductDTO;
 import br.com.youtube.product_ms.service.ProductService;
+import jakarta.persistence.GeneratedValue;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -26,6 +26,21 @@ public class ProductController {
             return new ResponseEntity<>(response.get(), HttpStatus.CREATED);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/id}")
+    public ResponseEntity<ProductDTO> getById(@PathVariable("id") Long id) {
+        Optional<ProductDTO> response =  service.getById(id);
+
+        if (response.isPresent()) {
+            return ResponseEntity.ok(response.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
 
