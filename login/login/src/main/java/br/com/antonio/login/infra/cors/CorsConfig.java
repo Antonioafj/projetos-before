70d7b328876083b2f7d1c45d6150ce56ddbe678d
Antonioafj <antonioafj.edu@gmail.com>
2025-09-23 02:48:42 -0400

    commit vendas e cors

--- login/login/src/main/java/br/com/antonio/login/infra/cors/CorsConfig.java
+++ login/login/src/main/java/br/com/antonio/login/infra/cors/CorsConfig.java
@@ -1,4 +1,15 @@
 package br.com.antonio.login.infra.cors;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig  implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST");
+    }
 }
