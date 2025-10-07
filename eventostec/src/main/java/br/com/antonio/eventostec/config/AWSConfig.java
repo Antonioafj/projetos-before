05cadf41a77c780373e28239517178e1d5baad23
Antonioafj <antonioafj.edu@gmail.com>
2025-10-06 22:10:58 -0400

    feat: create build cp arquiva jar para ec2 AWS

--- eventostec/src/main/java/br/com/antonio/eventostec/config/AWSConfig.java
+++ eventostec/src/main/java/br/com/antonio/eventostec/config/AWSConfig.java
@@ -3,7 +3,7 @@ package br.com.antonio.eventostec.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider; // <--- MUDANÇA AQUI
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -16,7 +16,7 @@ public class AWSConfig {
     @Bean
     public S3Client createS3Instance() {
         return S3Client.builder()
-                .credentialsProvider(ProfileCredentialsProvider.create("default"))
+                .credentialsProvider(InstanceProfileCredentialsProvider.create())
                 .region(Region.of(awsRegion))
                 .build();
     }
--- eventostec/src/test/java/br/com/antonio/eventostec/EventostecApplicationTests.java
+++ eventostec/src/test/java/br/com/antonio/eventostec/EventostecApplicationTests.java
@@ -2,8 +2,17 @@ package br.com.antonio.eventostec;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=password",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.flyway.enabled=false"
+})
 class EventostecApplicationTests {
 
 	@Test
