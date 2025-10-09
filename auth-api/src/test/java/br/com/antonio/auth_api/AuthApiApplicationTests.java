bc2192846c019a94c4d5b78e618d774db2475bd1
Antonioafj <antonioafj.edu@gmail.com>
2025-10-09 00:44:49 -0400

    feat: create security in java project with auth0 jwt other

---
+++ auth-api/src/test/java/br/com/antonio/auth_api/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antonio.auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
